2ed0a66237905af4e4e5eb33590677d88bcc11f5
omarlinp <omarlinp@gmail.com>
2026-01-23 08:30:37 -0400

    updated the changes to the dotnetdebugging code

--- 01/DotNetDebugging/Program.cs
+++ 01/DotNetDebugging/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System.Diagnostics;
 
-Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
-Debug.WriteLine($"We are looking for the {n}th number");
+
 
 int result = Fibonacci(5);
 Console.WriteLine(result);
 
 static int Fibonacci(int n)
 {
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
     int n1 = 0;
     int n2 = 1;
     int sum;
