3829cdd4c6cd85aeec1176f4d45272f7bc24411f
omarlinp <omarlinp@gmail.com>
2026-02-05 17:52:28 -0400

    app launched

--- 04/ASP_NET MVC/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ 04/ASP_NET MVC/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b494fb816f978c04d34ae64828c15249a5240787")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b551d2f6cb3867fa4ec0a786c27f958f87fa88a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
