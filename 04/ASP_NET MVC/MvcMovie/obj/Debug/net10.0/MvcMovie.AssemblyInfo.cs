692b816ceaa4c6803a047a5255b275767c54cac0
omarlinp <omarlinp@gmail.com>
2026-02-05 22:04:07 -0400

    completed the "Work with a database"

--- 04/ASP_NET MVC/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ 04/ASP_NET MVC/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c7ae283520521e9d39ea6386a9c048084510be9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91df226e62fc828c77726f11224779a89a6eeb58")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
