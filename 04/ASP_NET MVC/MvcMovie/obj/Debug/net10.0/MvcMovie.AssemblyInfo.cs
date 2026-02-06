3aaffddd0a624f06211339b277b1ee68ac9985cc
omarlinp <omarlinp@gmail.com>
2026-02-06 17:43:40 -0400

    completed the "add a new field" tutorial

--- 04/ASP_NET MVC/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ 04/ASP_NET MVC/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34965a673d33feb2da4a1c61ba399b4c47eb9a17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ed44b7b7e36efede146772b412967d317d030d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
